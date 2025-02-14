5e5521030547c7014284c44668852dd25ba539a7
Pushkarev.EE <lvstcentury@gmail.com>
2025-02-14 13:56:05 +0700

    first commit

---
+++ src/main/java/ru/caranga/Main.java
@@ -0,0 +1,23 @@
+package ru.caranga;
+
+public class Main {
+    public static void main(String[] args) {
+        // целые числа
+        byte varByte = 1; // -128 .. 127
+        short varShort = 1; // -32768 .. 32767
+        int varInt = 1; //
+        long varLong = 1L; //
+        // числа с плавающей точкой
+        float varFloat = 0.0f;
+        double varDouble = 0.0d; // must have
+        // логический
+        boolean varBool = true;
+        // символьный
+        char varChar = 'a';
+
+        String varStr = "abc"; //abc - строковый литерал
+
+
+        System.out.println("Hello, World!");
+    }
+}
