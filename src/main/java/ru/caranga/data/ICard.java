af96f3da4e99d4375e8368159da7b6a66ccce895
Pushkarev.EE <lvstcentury@gmail.com>
2025-03-23 17:51:33 +0700

    add comment line in ICard

--- src/main/java/ru/caranga/data/ICard.java
+++ src/main/java/ru/caranga/data/ICard.java
@@ -1,6 +1,7 @@
 package ru.caranga.data;
 
 //Все методы должны быть ПУБЛИЧНЫМИ. Определяет какой будет метод без наполнения (тела).
+//У интерфейса нет СОСТОЯНИЯ, он должен описывать только поведение.
 
 public interface ICard {
     public void payInCountry(Country country, int amount);
